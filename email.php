29d124b88841fa4e04205f9fc7b7c44dd74f5a19
Saif ansari <asaif332@gmail.com>
2018-09-05 23:23:47 +0530

    email configuration

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = Array(
+    'protocol' => '',
+    'smtp_host' => '',
+    'smtp_port' => ,
+    'smtp_user' => '',
+    'smtp_pass' => '',
+    'mailtype'  => 'html', 
+    'crlf' => "\r\n",
+    'newline' => "\r\n"
+  );
+
+
+?>
+
